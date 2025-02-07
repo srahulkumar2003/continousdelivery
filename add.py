843bebc46ed0202908a8d4d6e74e8c0a03e5d9f5
Your Name <you@example.com>
2025-02-07 16:53:36 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,37 @@
+# This program adds two numbers
+numl = 1.5
+num2 = 6.3
+# Add two numbers
+sum = numl + num2
+# Display the sum
+print('The sum of {O} and {1} is {2}' . format(numl,
+num2 ,
+sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
